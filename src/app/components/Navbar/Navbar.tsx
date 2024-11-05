4bd36945422713096af964e2a0daf5f24ff42796
Pxnque <panque08@outlook.es>
2024-11-05 01:13:35 -0600

    fixed landing page links on icon and inicio

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -10,13 +10,17 @@ const Navbar = () => {
     
       <header className=" bg-black">
         <nav className="flex justify-between items-center w-[92%] mx-auto">
+          
           <div>
+            <a href="/">
             <Image src={ImagenLogin} alt='logo' className='w-16 cursor-pointer'/>
+            </a>
           </div>
+
           <div className="nav-links duration-500 md:static absolute md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
             <ul className="text-white flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 font-montserrat">
               <li>
-                <Link href="/Page" className="hover:text-gray-200">INICIO</Link>
+                <Link href="/" className="hover:text-gray-200">INICIO</Link>
               </li>
               <li>
                 <Link href="/Homepage" className="hover:text-gray-200">HOMBRE</Link>
